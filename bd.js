91b952f812997e62bfec7938c4db75e051e62111
Daniel <72696191+deusnotam@users.noreply.github.com>
2024-02-17 12:59:59 +0100

    Update bd.js

--- bd.js
+++ bd.js
@@ -7,15 +7,15 @@
 
 // Проверка, был ли ранее подключен файл
 if (!window.D3usN0tamProtect) {
-
-  const sites = site;
   
   console.log("D3usN0tam System - Connected!\n\nMade by D3us N0tam\nNotion Site → https://deusnotam.notion.site/D3usN0tam-Protect-ba149f69de214fd3ba0b9df834eb2c6e?pvs=4\nTelegram → https://t.me/d3usn0tam");
 
   // Подключение datasite.js - база данных сайтов
-  var dataScript = document.createElement('script');
-  dataScript.src = 'https://deusnotam.github.io/datasite.js';
-  document.head.appendChild(dataScript);
+  var datasiteScript = document.createElement('script');
+  datasiteScript.src = 'https://deusnotam.github.io/datasite.js';
+  document.head.appendChild(datasiteScript);
+
+  const sites = site;
 
   // Подключение activator.js - скрипт проверки подключения
   var activatorScript = document.createElement('script');
