066dce092344735abcc7ad141b7c94a203c24200
Daniel <72696191+deusnotam@users.noreply.github.com>
2024-02-17 21:35:22 +0100

    Update bd.js

--- bd.js
+++ bd.js
@@ -29,7 +29,10 @@ if (!window.D3usN0tamProtect) {
     // Проверка, есть ли текущий домен в списке
     console.log("Current Domain:", currentDomain);
 
-    const siteInfo = sites.find(site => currentDomain === new URL(site.url).host);
+    const siteInfo = sites.find(site => {
+        const siteHostname = new URL(site.url).hostname;
+        return currentDomain === siteHostname || currentDomain === "www." + siteHostname || "www." + currentDomain === siteHostname;
+    });
 
     console.log("Site Info:", siteInfo);
 
