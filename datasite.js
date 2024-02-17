a1e3574e58e6608f9a3b1ac686a922eacdeb5bee
Daniel <72696191+deusnotam@users.noreply.github.com>
2024-02-17 12:13:42 +0100

    Create datasite.js

---
+++ datasite.js
@@ -0,0 +1,21 @@
+/*!
+ * DataSite.js
+ * by Deus Notam
+ * Telegram → https://t.me/d3usn0tam
+ * Доступные причины:
+ * didnt pay - не оплата работы
+ * Доступные эффекты:
+ * redirect - редирект на redirecturl
+ * thanos - поверх сайта чёрный экран с gif щелком таноса
+ */
+var data = [
+    {name: "ABROS",
+     url: "https://abros.me",
+     date: "09/01/2022",
+     note: "Это сайт для демонстрации работы данной модификации.",
+        protect: "active",
+           cause: "none",
+           effect: "none",
+           redirecturl: "-",
+    },
+];
