8594943d862a8faf6e05e263fe6f45ee3709eeee
Daniel <72696191+deusnotam@users.noreply.github.com>
2024-02-23 17:38:35 +0100

    Update datasite.js

--- datasite.js
+++ datasite.js
@@ -12,17 +12,17 @@
  */
 var site = [
     
-    {name: "ABROS",
-     url: "https://abros.me",
-     date: "09/01/2022",
-        noti: "active",
-           noti_title: "Тестовый сайт",
-           notibody: "Это сайт для демонстрации работы системы D3usN0tam.",
-        blocker: "active",
-           blocker_date: "01/01/2022",
-           blocker_cause: "didnt pay",
-           blocker_effect: "thanos",
-           blocker_redirecturl: "https://www.notion.so/deusnotam/D3usN0tam-System-ba149f69de214fd3ba0b9df834eb2c6e",
-        deusid: "active",
+    {name: 'ABROS',
+     url: 'https://abros.me',
+     date: '09/01/2022',
+        noti: 'active',
+           noti_title: 'Тестовый сайт',
+           noti_text: 'Данный сайт в черном списке<br>Подробности на <a style="color: #32a6ff;" href="https://codepen.io/cleveryeti/pen/JjwNqgP" target="_blank">сайте</a> =',
+        blocker: 'active',
+           blocker_date: '01/01/2022',
+           blocker_cause: 'didnt pay',
+           blocker_effect: 'thanos',
+           blocker_redirecturl: 'https://www.notion.so/deusnotam/D3usN0tam-System-ba149f69de214fd3ba0b9df834eb2c6e',
+        deusid: 'active',
     }
 ];
