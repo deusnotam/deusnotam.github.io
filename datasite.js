a83866ff64f50645efbed32c1dae780953bf685d
Daniel <72696191+deusnotam@users.noreply.github.com>
2024-02-23 18:00:42 +0100

    Update datasite.js

--- datasite.js
+++ datasite.js
@@ -17,7 +17,7 @@ var site = [
      date: '09/01/2022',
         noti: 'active',
            noti_title: 'Тестовый сайт',
-           noti_text: 'Данный сайт в черном списке<br>Подробности на <a style="color: #32a6ff;" href="https://codepen.io/cleveryeti/pen/JjwNqgP" target="_blank">сайте</a> =',
+           noti_text: 'Данный сайт в черном списке<br>Подробности на <a style="color: #32a6ff;" href="https://codepen.io/cleveryeti/pen/JjwNqgP" target="_blank">сайте</a>',
         blocker: 'active',
            blocker_date: '01/01/2022',
            blocker_cause: 'didnt pay',
