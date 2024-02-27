d4780edffb7f40d8ceb290673f71c0c30fbacb71
D3usN0tam <72696191+deusnotam@users.noreply.github.com>
2024-02-27 19:53:05 +0100

    Update datasite.js

--- datasite.js
+++ datasite.js
@@ -21,9 +21,13 @@ const headers = {
   Authorization: `Bearer ${apiKey}`,
 };
 
-// Выполняем запрос к Airtable API
 fetch(airtableApiUrl, { headers })
-  .then(response => response.json())
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
   .then(data => {
     // Проверка наличия данных
     if (data && data.records) {
