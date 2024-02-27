ac9a0639e53110e51b55d3bc4918213150141694
D3usN0tam <72696191+deusnotam@users.noreply.github.com>
2024-02-27 21:32:18 +0100

    Update datasite.js

--- datasite.js
+++ datasite.js
@@ -21,26 +21,25 @@ const options = {
 fetch('https://app.nocodb.com/api/v1/db/data/noco/p2kmbphsgvqs8kz/mpqof3e6f1ueozo/views/vwz1zne8sfxvhxco?offset=0&limit=25&where=', options)
     .then(response => response.json())
     .then(data => {
-        if (data && typeof data === 'object') {
-            // Преобразование данных в нужный формат, предположим, что это объект
-            var site = {
-                name: data.SiteName,
-                url: data.SiteURL,
-                date: data.Subscribe,
-                noti: data.Noti,
-                noti_title: data.NotiTitle,
-                noti_text: data.NotiText,
-                blocker: data.Blocker,
-                blocker_effect: data.BlockerEffect,
-                blocker_redirecturl: data.BlockerRedirectURL,
-                blocker_note: data.BlockerNote,
-            };
+        if (data && data.list && Array.isArray(data.list)) {
+            // Преобразование данных в нужный формат
+            const siteList = data.list.map(item => ({
+                name: item.SiteName,
+                url: item.SiteURL,
+                date: item.Subscribe,
+                noti: item.Noti,
+                noti_title: item.NotiTitle,
+                noti_text: item.NotiText,
+                blocker: item.Blocker,
+                blocker_effect: item.BlockerEffect,
+                blocker_redirecturl: item.BlockerRedirectURL,
+                blocker_note: item.BlockerNote,
+            }));
 
-            // Теперь 'site' содержит данные из вашей таблицы NocoDB
-            console.log(data);
-            console.log(site);
+            // Теперь 'siteList' содержит данные из вашей таблицы NocoDB
+            console.log(siteList);
         } else {
-            console.error('Ошибка: Полученные данные не являются объектом');
+            console.error('Ошибка: Полученные данные не соответствуют ожидаемой структуре');
         }
     })
     .catch(error => console.error('Ошибка при получении данных:', error));
