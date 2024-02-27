0a24f63f642610a86da87293886a812678585f1e
D3usN0tam <72696191+deusnotam@users.noreply.github.com>
2024-02-27 20:18:42 +0100

    Update datasite.js

--- datasite.js
+++ datasite.js
@@ -15,16 +15,13 @@ const apiKey = 'patuoL9R4t4wpFWXS';
 const baseId = 'appyM5LkcacbXYVGh';
 const tableName = 'Site';
 
-// URL для запросов к API Airtable
 const apiUrl = `https://api.airtable.com/v0/${baseId}/${tableName}`;
 
-// Заголовки запроса, включая API ключ
 const headers = new Headers({
   'Authorization': `Bearer ${apiKey}`,
   'Content-Type': 'application/json',
 });
 
-// Выполняем запрос к Airtable API
 fetch(apiUrl, { method: 'GET', headers: headers })
   .then(response => {
     if (!response.ok) {
@@ -33,17 +30,12 @@ fetch(apiUrl, { method: 'GET', headers: headers })
     return response.json();
   })
   .then(data => {
-    // Получаем данные из ответа
     const records = data.records;
 
-    // Обрабатываем данные (здесь можно добавить логику обработки)
-
-    // Ваш код для использования данных из Airtable
     var site = records.map(record => ({
       name: record.fields.Name,
       url: record.fields.URL,
       date: record.fields.Date,
-      // Добавьте другие поля по необходимости
     }));
 
     console.log(site);
