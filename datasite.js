a134414936a7d428c0ffa6bd71d289d4e10ac803
D3usN0tam <72696191+deusnotam@users.noreply.github.com>
2024-02-27 21:48:39 +0100

    Update datasite.js

--- datasite.js
+++ datasite.js
@@ -11,10 +11,18 @@
  * thanos - поверх сайта чёрный экран с gif щелчком таноса
  */
 
+// Получение токена из GitHub Secrets
+const authToken = process.env.XC_AUTH_TOKEN;
+
+if (!authToken) {
+    console.error('Ошибка: Токен не найден в GitHub Secrets.');
+    process.exit(1);
+}
+
 const options = {
     method: 'GET',
     headers: {
-        'xc-auth': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRhbnlhYWJyb3NAZ21haWwuY29tIiwiZGlzcGxheV9uYW1lIjoiRGFuaWVsIEFicm9zIiwiYXZhdGFyIjpudWxsLCJ1c2VyX25hbWUiOm51bGwsImlkIjoidXM1cGE1a3FlYmx6MjBxOSIsInJvbGVzIjoib3JnLWxldmVsLXZpZXdlciIsInRva2VuX3ZlcnNpb24iOiI4MjMyYzQ5ZGJhNjhhOTRlYTg0MGY5ZGUwOTljMDQ1NTQ0YjA2MGRjNDExNTFlYWZhMzEwNzM1OGE2M2MwNjI1MWExOTQwMGI0MzM5ZTdiOCIsInByb3ZpZGVyIjoiY29nbml0byIsImlhdCI6MTcwOTA2MzE3MSwiZXhwIjoxNzA5MDk5MTcxfQ.mjkJTW5DiVDwtGvzxGw6cLVY2PLM4w3tFUMEKQKi6B4'
+        'xc-auth': authToken
     }
 };
 
@@ -35,11 +43,3 @@ fetch('https://app.nocodb.com/api/v1/db/data/noco/p2kmbphsgvqs8kz/mpqof3e6f1ueoz
                 blocker_redirecturl: item.BlockerRedirectURL,
                 blocker_note: item.BlockerNote,
             }));
-
-            // Теперь 'siteList' содержит данные из вашей таблицы NocoDB
-            console.log(siteList);
-        } else {
-            console.error('Ошибка: Полученные данные не соответствуют ожидаемой структуре');
-        }
-    })
-    .catch(error => console.error('Ошибка при получении данных:', error));
