59b9073b0e293a1f6effa4dec501c317d8aa834d
D3usN0tam <72696191+deusnotam@users.noreply.github.com>
2024-02-28 10:02:10 +0100

    Update bd.js

--- bd.js
+++ bd.js
@@ -5,24 +5,49 @@
  * Telegram → https://t.me/d3usn0tam
  */
 
-// Проверка, был ли ранее подключен файл
+// Проверяем, был ли ранее подключен файл
 if (!window.D3usN0tam) {
   console.log("D3usN0tam System - Connected!\n\nMade by D3us N0tam\nNotion Site → https://deusnotam.notion.site/D3usN0tam-System-ba149f69de214fd3ba0b9df834eb2c6e?pvs=4\nTelegram → https://t.me/d3usn0tam");
 
-  // Подключение activator.js - скрипт проверки подключения
-  var activatorScript = document.createElement('script');
+  const options = {
+    method: 'GET',
+    headers: {
+        'xc-auth': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRhbnlhYWJyb3NAZ21haWwuY29tIiwiZGlzcGxheV9uYW1lIjoiRGFuaWVsIEFicm9zIiwiYXZhdGFyIjpudWxsLCJ1c2VyX25hbWUiOm51bGwsImlkIjoidXM1cGE1a3FlYmx6MjBxOSIsInJvbGVzIjoib3JnLWxldmVsLXZpZXdlciIsInRva2VuX3ZlcnNpb24iOiI4MjMyYzQ5ZGJhNjhhOTRlYTg0MGY5ZGUwOTljMDQ1NTQ0YjA2MGRjNDExNTFlYWZhMzEwNzM1OGE2M2MwNjI1MWExOTQwMGI0MzM5ZTdiOCIsInByb3ZpZGVyIjoiY29nbml0byIsImlhdCI6MTcwOTEwOTc4NiwiZXhwIjoxNzA5MTQ1Nzg2fQ.j0pPJRGk0_WDsHLmXaC9BkLYqH4AGKzvY4yDsUA65UE'
+    }
+  };
+
+  fetch('https://app.nocodb.com/api/v1/db/data/noco/p2kmbphsgvqs8kz/mpqof3e6f1ueozo/views/vwz1zne8sfxvhxco', options)
+    .then(response => response.json())
+    .then(data => {
+        if (data && data.list && Array.isArray(data.list)) {
+            // Преобразование данных в нужный формат
+            const DNsite = data.list.map(item => ({
+                name: item.SiteName,
+                url: item.SiteURL,
+                date: item.Subscribe,
+                noti: item.Noti,
+                noti_title: item.NotiTitle,
+                noti_text: item.NotiText,
+                blocker: item.Blocker,
+                blocker_effect: item.BlockerEffect,
+                blocker_redirecturl: item.BlockerRedirectURL,
+                blocker_note: item.BlockerNote,
+            }));
+
+            // Теперь 'DNsite' содержит данные из вашей таблицы NocoDB
+            console.log(DNsite);
+            // Здесь 'DNsite' является массивом, чтобы получить доступ к данным, нужно использовать индекс
+            console.log(DNsite[0].name);
+        } else {
+            console.error('Ошибка: Полученные данные не соответствуют ожидаемой структуре');
+        }
+    })
+    .catch(error => console.error('Ошибка при получении данных:', error));
+
+  // Подключаем activator.js - скрипт проверки подключения
+  const activatorScript = document.createElement('script');
   activatorScript.src = 'https://deusnotam.github.io/activator.js';
   document.head.appendChild(activatorScript);
-
-  document.addEventListener('DOMContentLoaded', function() {
-    // Подключение datasite.js - база данных сайтов
-    var datasiteScript = document.createElement('script');
-    datasiteScript.src = 'https://deusnotam.github.io/datasite.js';
-    datasiteScript.onload = function() {
-      checkDomain(); // Вызов функции для проверки домена после загрузки datasite.js
-    };
-    document.head.appendChild(datasiteScript);
-  });
 
   // Функция для проверки домена
   function checkDomain() {
@@ -30,7 +55,7 @@ if (!window.D3usN0tam) {
 
     const currentDomain = window.location.hostname;
 
-    // Проверка, есть ли текущий домен в списке
+    // Проверяем, есть ли текущий домен в списке
     const DeusSiteInfo = sites.find(site => {
       const siteHostname = new URL(site.url).hostname;
       return (
@@ -41,20 +66,20 @@ if (!window.D3usN0tam) {
     });
 
     if (!DeusSiteInfo) {
-      // Обработка случая, когда домен не найден в списке
+      // Обрабатываем случай, когда домен не найден в списке
       console.log("Сайт не найден в списке системы D3usN0tam.\nThe site was not found in the D3usN0tam system list.");
     } else {
-      // Проверка, если blocker у домена равен "active"
+      // Проверяем, если blocker у домена равен "active"
       if (DeusSiteInfo.blocker === "active") {
-        // Подключение blocker.js - скрипт наказаний
-        var blockerScript = document.createElement('script');
+        // Подключаем blocker.js - скрипт наказаний
+        const blockerScript = document.createElement('script');
         blockerScript.src = 'https://deusnotam.github.io/system/blocker.js';
         document.head.appendChild(blockerScript);
       }
-      // Проверка, если noti у домена равен "active"
+      // Проверяем, если noti у домена равен "active"
       if (DeusSiteInfo.noti === "active") {
-        // Подключение noti.js - скрипт уведомлений
-        var deusidScript = document.createElement('script');
+        // Подключаем noti.js - скрипт уведомлений
+        const deusidScript = document.createElement('script');
         deusidScript.src = 'https://deusnotam.github.io/system/noti.js';
         document.head.appendChild(deusidScript);
       }
